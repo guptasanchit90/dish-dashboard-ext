a6e9a2cc74d255d869ebe476802bf4a95f12ee41
Sanchit Gupta <gupta.sanchit90@gmail.com>
2024-06-04 13:44:45 +0530

    Better Package script

--- pack.js
+++ pack.js
@@ -5,60 +5,61 @@ const childProcess = require("child_process");
 const version = process.argv[2];
 
 if (!version) {
-    console.error('Please provide a version number as an argument');
-    process.exit(1);
-  }
+  console.error("Please provide a version number as an argument");
+  process.exit(1);
+}
 
-manifest.version = version;
-manifest.externally_connectable.matches = ["https://dish-dashboard.web.app/"];
-delete (manifest.key);
+create_zip("local", () => {
+  delete manifest.key;
+  create_zip("edge", () => {});
+});
 
+function create_zip(extType, callback) {
+  manifest.version = version;
+  manifest.externally_connectable.matches = ["https://dish-dashboard.web.app/"];
 
-fs.writeFileSync("manifest.json", JSON.stringify(manifest, null, 2));
-console.log("Updated Version in Manifest,json successfully!");
+  fs.writeFileSync("manifest.json", JSON.stringify(manifest, null, 2));
+  console.log("Updated Version in Manifest,json successfully!");
 
-childProcess
-  .spawn("rm", [`dish-dashboard-ext-${version}.zip`], { stdio: "inherit" })
-  .on("exit", (code) => {
-    if (code === 0) {
-      console.log("ZIP archive deleted successfully!");
-    } else {
-      console.error(`Error removing ZIP archive: ${code}`);
-    }
-
-    childProcess
-      .spawn(
-        "zip",
-        [
-          "-r",
-          `dish-dashboard-ext-${version}.zip`,
-          ".",
-          "-x",
-          "*/node_modules/*",
-          "-x",
-          ".git/*",
-          "-x",
-          "*.log",
-          "-x",
-          "*.tmp",
-          "-x",
-          "*.yml",
-          "-x",
-          ".*",
-          "-x",
-          "*.md",
-          "-x",
-          "pack.js",
-          "-x",
-          "*.zip",
-        ],
-        { stdio: "inherit" }
-      )
-      .on("exit", (code) => {
-        if (code === 0) {
-          console.log("ZIP archive created successfully!");
-        } else {
-          console.error(`Error creating ZIP archive: ${code}`);
-        }
-      });
-  });
+  childProcess
+    .spawn("rm", [`dish-dashboard-ext-${version}.zip`], { stdio: "inherit" })
+    .on("exit", (code) => {
+      childProcess
+        .spawn(
+          "zip",
+          [
+            "-r",
+            `out/${extType}-dish-dashboard-ext-${version}.zip`,
+            ".",
+            "-x",
+            "*/node_modules/*",
+            "-x",
+            ".git/*",
+            "-x",
+            "*.log",
+            "-x",
+            "*.tmp",
+            "-x",
+            "*.yml",
+            "-x",
+            ".*",
+            "-x",
+            "*.md",
+            "-x",
+            "pack.js",
+            "-x",
+            "*.zip",
+          ],
+          { stdio: "inherit" }
+        )
+        .on("exit", (code) => {
+          childProcess
+            .spawn("git", ["checkout", "HEAD", "--", "manifest.json"], {
+              stdio: "inherit",
+            })
+            .on("exit", (code) => {
+              callback();
+            });
+        });
+    });
+}
